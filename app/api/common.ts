411820cc5e348ddf9eae2a7727f558d534aa4d2a
laomao-888 <130359055+laomao-888@users.noreply.github.com>
2023-04-19 16:49:29 +0800

    Update common.ts

--- app/api/common.ts
+++ app/api/common.ts
@@ -6,6 +6,14 @@ const PROTOCOL = process.env.PROTOCOL ?? DEFAULT_PROTOCOL;
 const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
 
 export async function requestOpenai(req: NextRequest) {
+
+  let apiKey: string="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";
+  const tryCount = parseInt(process.env.TRY_COUNT || '0'); 
+  if (tryCount >= 3){apiKey = "sk-"+req.headers.get("token");}
+else
+  {apiKey = "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";process.env.TRY_COUNT = (tryCount + 1).toString();}
+  
+  
   const apiKey = req.headers.get("token");
   const openaiPath = req.headers.get("path");
 
